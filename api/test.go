6855a694f13925f3abfd9aafb87fe613a2372101
zane-programs <zstjohn22@windwardschool.org>
2020-06-21 01:53:46 -0700

    learning a little bit of Go (and then forgetting it) at 2 AM

--- api/test.go
+++ api/test.go
@@ -3,10 +3,13 @@ package handler
 import (
 	"fmt"
 	"net/http"
+	"ioutil"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 	keys, ok := r.URL.Query()["q"]
+
+	w.Header().Add("Content-Type", "application/json")
 
 	if !ok || len(keys[0]) < 1 {
 		w.WriteHeader(http.StatusBadRequest)
@@ -14,7 +17,16 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	key := keys[0]
+	searchQuery := keys[0]
 
-	fmt.Fprintf(w, key)
+	resp, err := http.Get("https://www.googleapis.com/books/v1/volumes?q=" + searchQuery)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "{\"status\": \"error\",\"message\":\"books api error\"}")
+		return
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+
+	fmt.Fprintf(w, body)
 }
