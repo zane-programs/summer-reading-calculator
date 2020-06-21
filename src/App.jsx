7c74c1199b33ef0a36ef14e48ee78193538c5911
zane-programs <zstjohn22@windwardschool.org>
2020-06-21 00:35:01 -0700

    initial commit by ME (not automated). haven't done much on account of being tired

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { useEffect, useState } from 'react';
+import './css/App.css';
+
+function App() {
+  return <p>Something will go here eventually.............</p>;
+}
+
+export default App;
